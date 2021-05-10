d0c78f8d485327db43755c7e3fc8eec77fc1304d
xld1393679430 <x1393679430@outlook.com>
2021-05-10 20:23:12 +0800

    koa router

--- index.js
+++ index.js
@@ -1,56 +1,25 @@
 const Koa = require('koa')
 const fs = require('fs')
+const Router = require('koa-router')
 const logger = require('./middleware/logger')
+const HomeRouter = require('./router/home')
+const NotFountRouter = require('./router/404')
+const TodoRouter = require('./router/todo')
 
 const app = new Koa()
+const router = new Router()
 
 app.use(logger())
 
-const renderHtml = async (path) => {
-    return new Promise((resolve, reject) => {
-        const filePath = `./view/${path}`
-        fs.readFile(filePath, 'binary', (err, data) => {
-            console.log(data, 'data');
-            if (err) {
-                reject(err)
-            } else {
-                resolve(data)
-            }
-        })
-    })
-}
+router.use('/', HomeRouter.routes(), HomeRouter.allowedMethods())
+router.use('/todo', TodoRouter.routes(), TodoRouter.allowedMethods())
+router.use('/404', NotFountRouter.routes(), NotFountRouter.allowedMethods())
 
-const router = async (url) => {
-    let file = 'index.html'
-    switch (url) {
-        case '/':
-            file = 'index.html'
-            break
-        case '/index':
-            file = 'index.html'
-            break
-        case '/todo':
-            file = 'todo.html'
-            break
-        case '/404':
-            file = '404.html'
-            break
-        default:
-            file = '404.html'
-            break
-    }
-    console.log(file, 'file')
-    let page = await renderHtml(file)
-    return page
-}
+app.use(router.routes()).use(router.allowedMethods())
 
-
-app.use(async (ctx) => {
-    console.log(ctx.request.url, 'ctx')
-    const url = ctx.request.url
-    const html = await router(url)
-    ctx.body = html
-})
+// app.use(async ctx => {
+//     console.log(ctx.request.url, '1111');
+// })
 
 app.listen(3002, () => {
     console.log('koa start');
--- router/home.js
+++ router/home.js
@@ -1,7 +1,8 @@
-const Koa = require('koa')
 const Router = require('koa-router')
 
 const HomeRouter = new Router()
 HomeRouter.get('/', async ctx => {
     ctx.body = 'home'
 })
+
+module.exports = HomeRouter
