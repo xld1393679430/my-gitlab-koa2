ce466292e82a1168271c0245ce07b3ae44e72eb0
xld1393679430 <x1393679430@outlook.com>
2021-05-12 10:53:22 +0800

    格式化

--- utils/uploadFile.js
+++ utils/uploadFile.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-05-12 10:21:24
  * @LastEditors: liangdong.xu
- * @LastEditTime: 2021-05-12 10:32:04
+ * @LastEditTime: 2021-05-12 10:52:32
  * @FilePath: /my-gitlab-koa2/utils/uploadFile.js
  */
 const inspect = require('util').inspect
@@ -15,12 +15,12 @@ const Busboy = require('busboy')
  * @param  {string} dirname 目录绝对地址
  * @return {boolean}        创建目录结果
  */
-function mkdirsSync( dirname ) {
-  if (fs.existsSync( dirname )) {
+function mkdirsSync(dirname) {
+  if (fs.existsSync(dirname)) {
     return true
   } else {
-    if (mkdirsSync( path.dirname(dirname)) ) {
-      fs.mkdirSync( dirname )
+    if (mkdirsSync(path.dirname(dirname))) {
+      fs.mkdirSync(dirname)
       return true
     }
   }
@@ -31,7 +31,7 @@ function mkdirsSync( dirname ) {
  * @param  {string} fileName 获取上传文件的后缀名
  * @return {string}          文件后缀名
  */
-function getSuffixName( fileName ) {
+function getSuffixName(fileName) {
   let nameList = fileName.split('.')
   return nameList[nameList.length - 1]
 }
@@ -42,34 +42,34 @@ function getSuffixName( fileName ) {
  * @param  {object} options 文件上传参数 fileType文件类型， path文件存放路径
  * @return {promise}         
  */
-function uploadFile( ctx, options) {
+function uploadFile(ctx, options) {
   let req = ctx.req
   let res = ctx.res
-  let busboy = new Busboy({headers: req.headers})
+  let busboy = new Busboy({ headers: req.headers })
 
   // 获取类型
   let fileType = options.fileType || 'common'
-  let filePath = path.join( options.path,  fileType)
-  let mkdirResult = mkdirsSync( filePath )
-  
+  let filePath = path.join(options.path, fileType)
+  let mkdirResult = mkdirsSync(filePath)
+
   return new Promise((resolve, reject) => {
     console.log('文件上传中...')
-    let result = { 
+    let result = {
       success: false,
       formData: {},
     }
 
     // 解析请求文件事件
-    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+    busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
       // let fileName = Math.random().toString(16).substr(2) + '.' + getSuffixName(filename)
-      let _uploadFilePath = path.join( filePath, filename )
+      let _uploadFilePath = path.join(filePath, filename)
       let saveTo = path.join(_uploadFilePath)
 
       // 文件保存到制定路径
       file.pipe(fs.createWriteStream(saveTo))
 
       // 文件写入事件结束
-      file.on('end', function() {
+      file.on('end', function () {
         result.success = true
         result.message = '文件上传成功'
 
@@ -79,29 +79,29 @@ function uploadFile( ctx, options) {
     })
 
     // 解析表单中其他字段信息
-    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+    busboy.on('field', function (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
       console.log('表单字段数据 [' + fieldname + ']: value: ' + inspect(val));
       result.formData[fieldname] = inspect(val);
     });
 
     // 解析结束事件
-    busboy.on('finish', function( ) {
+    busboy.on('finish', function () {
       console.log('文件上结束')
       resolve(result)
     })
 
     // 解析错误事件
-    busboy.on('error', function(err) {
+    busboy.on('error', function (err) {
       console.log('文件上出错')
       reject(result)
     })
 
     req.pipe(busboy)
   })
-    
-} 
+
+}
 
 
-module.exports =  {
+module.exports = {
   uploadFile
 }
